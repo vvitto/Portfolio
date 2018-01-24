8fc69c758126d2ba6bd80e88d297767fedf7ae1d
vvitto <vvitto123@gmail.com>
2018-01-24 20:43:11 +0200

    Gitignore: set ignoring directory with works | Gulpfile: added ability to upload files to the hostring

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-var gulp        = require("gulp");
-var sass        = require("gulp-sass");
-var pug         = require("gulp-pug");
-var browserSync = require("browser-sync");
-var imagemin    = require("gulp-imagemin");
-var cache       = require("gulp-cache");
-var del         = require("del");
-var sequence    = require("run-sequence");
-var cssnano     = require("gulp-cssnano");
-var autoprefixer= require("gulp-autoprefixer");
+var gulp            = require("gulp");
+var sass            = require("gulp-sass");
+var pug             = require("gulp-pug");
+var browserSync     = require("browser-sync");
+var imagemin        = require("gulp-imagemin");
+var cache           = require("gulp-cache");
+var del             = require("del");
+var sequence        = require("run-sequence");
+var cssnano         = require("gulp-cssnano");
+var autoprefixer    = require("gulp-autoprefixer");
+var ftp             = require("vinyl-ftp");
+
 gulp.task("sass", function(){
     return gulp.src("src/sass/main.sass")
             .pipe(sass().on('error', sass.logError))
@@ -45,7 +47,7 @@ gulp.task("watch", ['browserSync', 'sass'], function(){
 
 
 gulp.task('image', function(){
-	return gulp.src("src/assets/**/*.+(png|jpg|jpeg|svg|gif|ico)")
+	return gulp.src(["src/assets/**/*.+(png|jpg|jpeg|svg|gif|ico)", "!src/assets/works/**/*"])
 		.pipe(cache(imagemin({
 			interlaced: true,
 			progressive: true,
@@ -61,18 +63,37 @@ gulp.task("fonts", function(){
 gulp.task("clean", function(){
 	return del.sync("dist");
 });
-
+3
 gulp.task("build", ['clean', 'fonts','image', 'sass', 'pug'], function(){
-    var buildHtml = gulp.src(['src/*.html'])
+    var buildHtml = gulp.src('src/*.html')
                     .pipe(gulp.dest("dist/"));
 
-    var buildCss = gulp.src(['src/css/*.css',])
+    var buildCss = gulp.src('src/css/*.css')
                     .pipe(cssnano())
                     .pipe(gulp.dest('dist/css'));
-
-	var buildFonts = gulp.src('src/assets/fonts/**/*')
-		.pipe(gulp.dest("dist/assets/fonts"));
+    
+    var works = gulp.src('src/assets/works/**/*')
+                    .pipe(gulp.dest('dist/assets/works'));
 
 	var buildJs = gulp.src('src/js/**/*')
-		.pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("dist/js"));
+    return;
+});
+
+gulp.task("ftp", function(){
+    var conn = ftp.create( {
+		host:     'uashared07.twinservers.net',
+		user:     'vvittona',
+		password: 'pO3f24o3Za',
+		parallel: 10
+	} );
+
+	var globs = [
+		'dist/**/*'
+	];
+
+
+	return gulp.src( globs, { base: 'dist/', buffer: false } )
+		.pipe( conn.newer( '/public_html' ) ) // only upload newer files
+		.pipe( conn.dest( '/public_html' ) );
 });
